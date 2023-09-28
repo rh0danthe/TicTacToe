af6ac22ed95990d2ac71faac2dad45a1ac6ca0fa
rh0danthe <rita.pichushkina.04@mail.ru>
2023-09-28 18:09:45 +0300

    fix: elements name change

--- TicTacToe/GameForm.cs
+++ TicTacToe/GameForm.cs
@@ -1,8 +1,8 @@
 namespace TicTacToe
 {
-    public partial class Form1 : Form
+    public partial class GameForm : Form
     {
-        public Form1()
+        public GameForm()
         {
             InitializeComponent();
         }

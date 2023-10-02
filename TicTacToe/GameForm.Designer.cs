626f3693b8e5a835edd45ef3a600a92a0790f2ec
rh0danthe <rita.pichushkina.04@mail.ru>
2023-10-03 00:05:05 +0300

    Added a function makeMove and appearance of symbols on play buttons (when pressed)

--- TicTacToe/GameForm.Designer.cs
+++ TicTacToe/GameForm.Designer.cs
@@ -44,6 +44,7 @@
             symbolChoice_label = new Label();
             firstPlayerChoice_label = new Label();
             secondPlayerChoice_label = new Label();
+            message_label = new Label();
             gameField_panel.SuspendLayout();
             symbolChoice_panel.SuspendLayout();
             SuspendLayout();
@@ -60,82 +61,119 @@
             gameField_panel.Controls.Add(rightLower);
             gameField_panel.Controls.Add(middleMiddle);
             gameField_panel.Controls.Add(leftUpper);
-            gameField_panel.Location = new Point(261, 114);
+            gameField_panel.Location = new Point(209, 91);
+            gameField_panel.Margin = new Padding(2);
             gameField_panel.Name = "gameField_panel";
-            gameField_panel.Size = new Size(281, 266);
+            gameField_panel.Size = new Size(225, 213);
             gameField_panel.TabIndex = 0;
             // 
             // middleLower
             // 
-            middleLower.Location = new Point(108, 180);
+            middleLower.FlatStyle = FlatStyle.Flat;
+            middleLower.Location = new Point(86, 144);
+            middleLower.Margin = new Padding(2);
             middleLower.Name = "middleLower";
-            middleLower.Size = new Size(64, 61);
+            middleLower.Size = new Size(51, 49);
             middleLower.TabIndex = 8;
+            middleLower.Tag = "7";
             middleLower.UseVisualStyleBackColor = true;
+            middleLower.Click += Play_button_Click;
             // 
             // leftLower
             // 
-            leftLower.Location = new Point(20, 180);
+            leftLower.FlatStyle = FlatStyle.Flat;
+            leftLower.Location = new Point(16, 144);
+            leftLower.Margin = new Padding(2);
             leftLower.Name = "leftLower";
-            leftLower.Size = new Size(64, 61);
+            leftLower.Size = new Size(51, 49);
             leftLower.TabIndex = 7;
+            leftLower.Tag = "6";
             leftLower.UseVisualStyleBackColor = true;
+            leftLower.Click += Play_button_Click;
             // 
             // leftMiddle
             // 
-            leftMiddle.Location = new Point(20, 103);
+            leftMiddle.FlatStyle = FlatStyle.Flat;
+            leftMiddle.Location = new Point(16, 82);
+            leftMiddle.Margin = new Padding(2);
             leftMiddle.Name = "leftMiddle";
-            leftMiddle.Size = new Size(64, 61);
+            leftMiddle.Size = new Size(51, 49);
             leftMiddle.TabIndex = 6;
+            leftMiddle.Tag = "3";
             leftMiddle.UseVisualStyleBackColor = true;
+            leftMiddle.Click += Play_button_Click;
             // 
             // rightMiddle
             // 
-            rightMiddle.Location = new Point(197, 103);
+            rightMiddle.FlatStyle = FlatStyle.Flat;
+            rightMiddle.Location = new Point(158, 82);
+            rightMiddle.Margin = new Padding(2);
             rightMiddle.Name = "rightMiddle";
-            rightMiddle.Size = new Size(64, 61);
+            rightMiddle.Size = new Size(51, 49);
             rightMiddle.TabIndex = 5;
+            rightMiddle.Tag = "5";
             rightMiddle.UseVisualStyleBackColor = true;
+            rightMiddle.Click += Play_button_Click;
             // 
             // rightUpper
             // 
-            rightUpper.Location = new Point(197, 22);
+            rightUpper.FlatStyle = FlatStyle.Flat;
+            rightUpper.Location = new Point(158, 18);
+            rightUpper.Margin = new Padding(2);
             rightUpper.Name = "rightUpper";
-            rightUpper.Size = new Size(64, 61);
+            rightUpper.Size = new Size(51, 49);
             rightUpper.TabIndex = 4;
+            rightUpper.Tag = "2";
             rightUpper.UseVisualStyleBackColor = true;
+            rightUpper.Click += Play_button_Click;
             // 
             // middleUpper
             // 
-            middleUpper.Location = new Point(108, 22);
+            middleUpper.FlatStyle = FlatStyle.Flat;
+            middleUpper.Location = new Point(86, 18);
+            middleUpper.Margin = new Padding(2);
             middleUpper.Name = "middleUpper";
-            middleUpper.Size = new Size(64, 61);
+            middleUpper.Size = new Size(51, 49);
             middleUpper.TabIndex = 3;
+            middleUpper.Tag = "1";
             middleUpper.UseVisualStyleBackColor = true;
+            middleUpper.Click += Play_button_Click;
             // 
             // rightLower
             // 
-            rightLower.Location = new Point(197, 180);
+            rightLower.FlatStyle = FlatStyle.Flat;
+            rightLower.Location = new Point(158, 144);
+            rightLower.Margin = new Padding(2);
             rightLower.Name = "rightLower";
-            rightLower.Size = new Size(64, 61);
+            rightLower.Size = new Size(51, 49);
             rightLower.TabIndex = 2;
+            rightLower.Tag = "8";
             rightLower.UseVisualStyleBackColor = true;
+            rightLower.Click += Play_button_Click;
             // 
             // middleMiddle
             // 
-            middleMiddle.Location = new Point(108, 103);
+            middleMiddle.FlatStyle = FlatStyle.Flat;
+            middleMiddle.Location = new Point(86, 82);
+            middleMiddle.Margin = new Padding(2);
             middleMiddle.Name = "middleMiddle";
-            middleMiddle.Size = new Size(64, 61);
+            middleMiddle.Size = new Size(51, 49);
             middleMiddle.TabIndex = 1;
+            middleMiddle.Tag = "4";
             middleMiddle.UseVisualStyleBackColor = true;
+            middleMiddle.Click += Play_button_Click;
             // 
             // leftUpper
             // 
-            leftUpper.Location = new Point(20, 22);
+            leftUpper.FlatStyle = FlatStyle.Flat;
+            leftUpper.Location = new Point(16, 18);
+            leftUpper.Margin = new Padding(2);
             leftUpper.Name = "leftUpper";
-            leftUpper.Size = new Size(64, 61);
+            leftUpper.Size = new Size(51, 49);
             leftUpper.TabIndex = 0;
-            leftUpper.UseVisualStyleBackColor = true;
+            leftUpper.Tag = "0";
+            leftUpper.UseVisualStyleBackColor = false;
+            leftUpper.Click += Play_button_Click;
             // 
             // symbolChoice_panel
             // 
@@ -143,15 +181,17 @@
             symbolChoice_panel.Controls.Add(xChoice_button);
             symbolChoice_panel.Controls.Add(symbolChoice_label);
             symbolChoice_panel.Location = new Point(0, 0);
+            symbolChoice_panel.Margin = new Padding(2);
             symbolChoice_panel.Name = "symbolChoice_panel";
-            symbolChoice_panel.Size = new Size(330, 108);
+            symbolChoice_panel.Size = new Size(264, 86);
             symbolChoice_panel.TabIndex = 1;
             // 
             // oChoice_button
             // 
-            oChoice_button.Location = new Point(158, 37);
+            oChoice_button.Location = new Point(126, 30);
+            oChoice_button.Margin = new Padding(2);
             oChoice_button.Name = "oChoice_button";
-            oChoice_button.Size = new Size(56, 51);
+            oChoice_button.Size = new Size(45, 41);
             oChoice_button.TabIndex = 10;
             oChoice_button.Text = "О";
             oChoice_button.UseVisualStyleBackColor = true;
@@ -159,9 +199,10 @@
             // 
             // xChoice_button
             // 
-            xChoice_button.Location = new Point(75, 37);
+            xChoice_button.Location = new Point(60, 30);
+            xChoice_button.Margin = new Padding(2);
             xChoice_button.Name = "xChoice_button";
-            xChoice_button.Size = new Size(56, 51);
+            xChoice_button.Size = new Size(45, 41);
             xChoice_button.TabIndex = 9;
             xChoice_button.Text = "Х";
             xChoice_button.UseVisualStyleBackColor = true;
@@ -170,37 +211,50 @@
             // symbolChoice_label
             // 
             symbolChoice_label.AutoSize = true;
-            symbolChoice_label.Location = new Point(12, 9);
+            symbolChoice_label.Location = new Point(10, 7);
+            symbolChoice_label.Margin = new Padding(2, 0, 2, 0);
             symbolChoice_label.Name = "symbolChoice_label";
-            symbolChoice_label.Size = new Size(289, 25);
+            symbolChoice_label.Size = new Size(243, 20);
             symbolChoice_label.TabIndex = 2;
             symbolChoice_label.Text = "Первый игрок, выберите символ:";
             // 
             // firstPlayerChoice_label
             // 
             firstPlayerChoice_label.AutoSize = true;
-            firstPlayerChoice_label.Location = new Point(12, 128);
+            firstPlayerChoice_label.Location = new Point(10, 102);
+            firstPlayerChoice_label.Margin = new Padding(2, 0, 2, 0);
             firstPlayerChoice_label.Name = "firstPlayerChoice_label";
-            firstPlayerChoice_label.Size = new Size(0, 25);
+            firstPlayerChoice_label.Size = new Size(0, 20);
             firstPlayerChoice_label.TabIndex = 2;
             // 
             // secondPlayerChoice_label
             // 
             secondPlayerChoice_label.AutoSize = true;
-            secondPlayerChoice_label.Location = new Point(557, 128);
+            secondPlayerChoice_label.Location = new Point(446, 102);
+            secondPlayerChoice_label.Margin = new Padding(2, 0, 2, 0);
             secondPlayerChoice_label.Name = "secondPlayerChoice_label";
-            secondPlayerChoice_label.Size = new Size(0, 25);
+            secondPlayerChoice_label.Size = new Size(0, 20);
             secondPlayerChoice_label.TabIndex = 3;
+            // 
+            // message_label
+            // 
+            message_label.AutoSize = true;
+            message_label.Location = new Point(230, 203);
+            message_label.Name = "message_label";
+            message_label.Size = new Size(0, 20);
+            message_label.TabIndex = 9;
             // 
             // GameForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(640, 360);
+            Controls.Add(message_label);
             Controls.Add(secondPlayerChoice_label);
             Controls.Add(firstPlayerChoice_label);
             Controls.Add(symbolChoice_panel);
             Controls.Add(gameField_panel);
+            Margin = new Padding(2);
             Name = "GameForm";
             Text = "TicTacGame";
             gameField_panel.ResumeLayout(false);
@@ -228,5 +282,6 @@
         private Button xChoice_button;
         private Label firstPlayerChoice_label;
         private Label secondPlayerChoice_label;
+        private Label message_label;
     }
 }
--- TicTacToe/GameForm.cs
+++ TicTacToe/GameForm.cs
@@ -5,16 +5,24 @@ namespace TicTacToe
         public GameForm()
         {
             InitializeComponent();
+            gameField_panel.Visible = false;
         }
+
+        private char firstPlayerSymbol = '\0';
+        private char secondPlayerSymbol = '\0';
+        private char currentPlayerSymbol;
+        private GameLogic gameLogic = new GameLogic();
 
         private void Choice_button_Click(object sender, EventArgs e)
         {
             Button senderButton = (Button)sender;
-            string symbol = senderButton.Text;
+            char symbol = senderButton.Text[0];
             if (firstPlayerChoice_label.Text == "")
             {
                 firstPlayerChoice_label.Text = "Символ первого игрока: " + symbol;
                 symbolChoice_label.Text = "Второй игрок, выберите символ";
+                firstPlayerSymbol = symbol;
+                currentPlayerSymbol = firstPlayerSymbol;
             }
             else if (firstPlayerChoice_label.Text == "Символ первого игрока: " + symbol)
                 MessageBox.Show("Этот символ уже занят", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -22,7 +30,28 @@ namespace TicTacToe
             {
                 secondPlayerChoice_label.Text = "Символ второго игрока: " + symbol;
                 symbolChoice_panel.Visible = false;
+                gameField_panel.Visible = true;
+                secondPlayerSymbol = symbol;
             }
+        }
+
+        private void Play_button_Click(object sender, EventArgs e)
+        {
+            Button senderButton = (Button)sender;
+            senderButton.Text = currentPlayerSymbol.ToString();
+            int senderTag = Convert.ToInt32(senderButton.Tag);
+            if (gameLogic.makeMove(senderTag, currentPlayerSymbol))
+            {
+                gameField_panel.Visible = false;
+                if (currentPlayerSymbol == firstPlayerSymbol)
+                    message_label.Text = "Выиграл первый игрок!";
+                else message_label.Text = "Выиграл второй игрок!";
+            }
+            senderButton.Enabled = false;
+            if (currentPlayerSymbol == firstPlayerSymbol)
+                currentPlayerSymbol = secondPlayerSymbol;
+            else
+                currentPlayerSymbol = firstPlayerSymbol;
         }
     }
 }
